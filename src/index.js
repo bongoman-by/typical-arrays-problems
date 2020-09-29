57b8da823bfa887d493378d0d9656b084236f9b4
bongoman-by <bongoman@tut.by>
2020-09-29 11:34:39 +0300

    feat: done typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (!checkArray(array)) {
+        return 0;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    function getMin(obj) {
+        let a = obj[0];
+        for (let i = 1; i < obj.length; i++) {
+            if (obj[i] < a) {
+                a = obj[i];
+            }
+        }
+        return a;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    return getMin(array);
+};
+
+exports.max = function max(array) {
+    
+    if (!checkArray(array)) {
+        return 0;
+    }
+
+
+    function getMax(obj) {
+        let a = obj[0];
+        for (let i = 1; i < obj.length; i++) {
+            if (obj[i] > a) {
+                a = obj[i];
+            }
+        }
+        return a;
+    }
+
+    return getMax(array);
+};
+
+exports.avg = function avg(array) {
+    
+    if (!checkArray(array)) {
+        return 0;
+    }
+
+    function getAvg(obj) {
+        let a = 0;
+        for (let i = 0; i < obj.length; i++) {
+                a = a + (+obj[i]);
+        }
+        return a / obj.length;
+    }
+
+    return getAvg(array);
+};
+
+const checkArray = (array) => {
+    if (array === null || array === undefined || isNan(array)) {
+        return false;
+    }
+
+    if (array.constructor !== Array) {
+        return false;
+    }
+
+    if (array.length === 0) {
+        return false;
+    }
+
+    return true;
+};
+
+function isNan(value) {
+    return value !== value;
 }
